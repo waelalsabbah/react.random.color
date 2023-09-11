061b9e66dc603b3685f80ea060590a8af9afe94a
waelalsabbah <alsabbahwael@gmail.com>
2023-09-11 23:14:35 +0200

    random color generated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import { useState } from 'react';
 export default function App() {
   const Initialcolor = randomColor();
   const [color, setColor] = useState('Generated Color');
+  const hue = 240;
+  const lightness = 75;
   return (
     <>
       <h1> Random Color Generator</h1>;
-      <div style={{ backgroundColor: '#72e3dc' }}>
+      <div
+        style={{
+          backgroundColor: `hsl(${hue}, 100%, ${lightness}%)`,
+          width: '100px',
+          height: '100px',
+        }}
+      >
         <br />
         {color}
       </div>
